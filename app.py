a610b6010ac3d94c9d84733f9620795f84313178
Dustin Updyke <dustin@updyke.com>
2016-04-02 12:24:51 -0400

    uses qs to pass abbvreviation

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import unirest
-
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 app.debug = True
 
 @app.route('/')
 def hello_world():
-	response = unirest.get("https://daxeel-abbreviations-v1.p.mashape.com/all/CS",
+	response = unirest.get("https://daxeel-abbreviations-v1.p.mashape.com/all/" + request.args.get('a'),
 	  headers={
-	    "X-Mashape-Key": "<key>"
+	    "X-Mashape-Key": "key"
 	  }
 	)    
 	return response.raw_body
